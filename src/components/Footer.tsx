c04cb10525545a3dffeec6259f2778bdb1449323
Kevin Fosse <kevin.fosse057@gmail.com>
2023-07-15 19:57:38 +0200

    Fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer>
+            <footer>
 
-            
-            <p>Made with <span style={{color: "red"}}>&hearts;</span> in France</p>
-            <a href="https://github.com/kevinfosse/pet-simulator">Virtuel Pet <img src="git" alt="" /> </a>
-            
+                
+                <p>Made with <span style={{color: "red"}}>&hearts;</span> in France</p>
+                <a href="https://github.com/kevinfosse/pet-simulator">Virtuel Pet <img src="git" alt="" /> </a>
+                
 
-        </footer>
+            </footer>
     )}
